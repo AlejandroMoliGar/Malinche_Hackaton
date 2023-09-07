013805425e69c8ec5d6c35dd7344e49f7301d528
AlejandroMoligar <alejandromolinagarcia3@gmail.com>
2023-09-07 10:40:36 -0600

    Modificaciones

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import openai
 google_api_key = "AIzaSyD_PYnX11iei21YE97L8fw6oz4cnWLpYns"
 google_cse_id = "0544d305b2ea34c29"
 
-openai.api_key = "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"
+openai.api_key = "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"
