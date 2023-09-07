d390327d80eeaa68c0b59df65478a5a2f1e53be2
AlejandroMoligar <alejandromolinagarcia3@gmail.com>
2023-09-07 10:44:49 -0600

    .

--- config.py
+++ config.py
@@ -3,9 +3,4 @@ import openai
 # Configura tu clave de API de Google Custom Search
 google_api_key = "AIzaSyD_PYnX11iei21YE97L8fw6oz4cnWLpYns"
 google_cse_id = "0544d305b2ea34c29"
-
-<<<<<<< HEAD
 openai.api_key = "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"
-=======
-openai.api_key = "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"
->>>>>>> 1f0f92f23c060f7d44086c77efe8b998590d57ee
