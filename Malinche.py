ac3016229f438002dfa85aafec5db4f392b779e1
AlejandroMoligar <alejandromolinagarcia3@gmail.com>
2023-09-06 20:41:57 -0600

    Solucion Falta Diceño

--- Malinche.py
+++ Malinche.py
@@ -2,5 +2,9 @@ import streamlit as st
 from maps import display_map
 from chatbot import chatbot
 
+# Habilitar acceso al micrófono y altavoces
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+# Llamar a las funciones
 display_map()
 chatbot()
